6710609c3686af6418f9dfad9e75eb38818a0a62
shiro <okano.03.remi@gmail.com>
2026-02-05 15:59:44 +0900

    商品グリッド上端位置修正（ホバー前ちょいズレ保留）

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -6,22 +6,20 @@
 
 @section('content')
 <div class="body">
-    {{-- ヘッダー下のメインコンテンツ --}}
     <div class="main-container">
         {{-- 左側：検索エリア --}}
         <div class="main-left">
             <div class="section__title">
                 <h2>商品一覧</h2>
             </div>
-            
             <form class="search-form" method="GET" action="{{ route('products.index') }}">
                 @csrf
                 <div class="search-form__item">
-                    <input class="search-form__item-input" 
-                           type="text" 
-                           name="search"
-                           placeholder="商品名で検索"
-                           value="{{ request('search') }}">
+                    <input class="search-form__item-input"
+                        type="text"
+                        name="search"
+                        placeholder="商品名で検索"
+                        value="{{ request('search') }}">
                 </div>
                 <div class="search-form__button">
                     <button class="search-form__button-submit" type="submit">検索</button>
@@ -35,58 +33,56 @@
                     </select>
                 </div>
             </form>
-            
             <hr class="divider">
         </div>
+        {{-- 右側：コンテンツエリア --}}
+        <div class="main-right">
+            {{-- 右上：追加ボタン --}}
+            <div class="add-product">
+                <a href="{{ route('products.register') }}" class="add-product__btn">＋商品を追加</a>
+            </div>
+            {{-- 商品グリッド --}}
+            <div class="product-grid">
+                @forelse($products as $product)
+                    <div class="product-card">
+                        <a href="{{ route('products.detail', $product['id']) }}" class="product-card__link">
+                            @if(isset($product['image_path']) && $product['image_path'])
+                                <img src="{{ asset('storage/' . $product['image_path']) }}"
+                                    alt="{{ $product['name'] }}"
+                                    class="product-card__image">
+                            @else
+                                <div class="product-card__no-image">画像なし</div>
+                            @endif
+                            <div class="product-card__content">
+                                <h3 class="product-card__name">{{ $product['name'] }}</h3>
+                                <p class="product-card__price">¥{{ number_format($product['price']) }}</p>
+                            </div>
+                        </a>
+                    </div>
+                @empty
+                    <p class="no-products">商品がありません</p>
+                @endforelse
+            </div>
+            {{-- ページネーション（配列の場合は手動実装） --}}
+            <div class="pagination">
+                @if(isset($currentPage) && isset($lastPage))
+                    @if($currentPage > 1)
+                        <a href="?page={{ $currentPage - 1 }}" class="pagination-link">＜</a>
+                    @endif
 
-        {{-- 右上：追加ボタン --}}
-        <div class="add-button-container">
-            <a href="{{ route('products.register') }}" class="add-product-btn">＋商品を追加</a>
-        </div>
+                    @for($i = 1; $i <= $lastPage; $i++)
+                        <a href="?page={{ $i }}"
+                        class="pagination-link {{ $currentPage == $i ? 'active' : '' }}">
+                            {{ $i }}
+                        </a>
+                    @endfor
 
-        {{-- 商品グリッド --}}
-        <div class="product-grid">
-            @forelse($products as $product)
-                <div class="product-card">
-                    <a href="{{ route('products.detail', $product['id']) }}" class="product-card__link">
-                        @if(isset($product['image_path']) && $product['image_path'])
-                            <img src="{{ asset('storage/' . $product['image_path']) }}" 
-                                alt="{{ $product['name'] }}" 
-                                class="product-card__image">
-                        @else
-                            <div class="product-card__no-image">画像なし</div>
-                        @endif
-                        <div class="product-card__content">
-                            <h3 class="product-card__name">{{ $product['name'] }}</h3>
-                            <p class="product-card__price">¥{{ number_format($product['price']) }}</p>
-                        </div>
-                    </a>
-                </div>
-            @empty
-                <p class="no-products">商品がありません</p>
-            @endforelse
-        </div>
-
-        {{-- ページネーション（配列の場合は手動実装） --}}
-        <div class="pagination">
-            @if(isset($currentPage) && isset($lastPage))
-                @if($currentPage > 1)
-                    <a href="?page={{ $currentPage - 1 }}" class="pagination-link">＜</a>
+                    @if($currentPage < $lastPage)
+                        <a href="?page={{ $currentPage + 1 }}" class="pagination-link">＞</a>
+                    @endif
                 @endif
-                
-                @for($i = 1; $i <= $lastPage; $i++)
-                    <a href="?page={{ $i }}" 
-                    class="pagination-link {{ $currentPage == $i ? 'active' : '' }}">
-                        {{ $i }}
-                    </a>
-                @endfor
-                
-                @if($currentPage < $lastPage)
-                    <a href="?page={{ $currentPage + 1 }}" class="pagination-link">＞</a>
-                @endif
-            @endif
+            </div>
         </div>
-
     </div>
 </div>
 @endsection
