bf8fecf58744ae15ba0a716b6fcc2c960deeaa24
shiro <okano.03.remi@gmail.com>
2026-02-06 17:26:30 +0900

    placeholder,必須ラベル

--- src/resources/views/products/register.blade.php
+++ src/resources/views/products/register.blade.php
@@ -12,23 +12,29 @@
         @csrf
 
         <div class="form-group">
-            <label class="form-label">商品名</label>
-            <input type="text" name="name" value="{{ old('name') }}">
+            <label class="form-label">
+                商品名 <span class="required">必須</span>
+            </label>
+            <input type="text" name="name" value="{{ old('name') }}" placeholder="商品名を入力">
             @error('name')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="form-group">
-            <label class="form-label">値段</label>
-            <input type="text" name="price" value="{{ old('price') }}" min="0">
+            <label class="form-label">
+                値段 <span class="required">必須</span>
+            </label>
+            <input type="text" name="price" value="{{ old('price') }}" placeholder="値段を入力">
             @error('price')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="form-group">
-            <label class="form-label">商品画像</label>
+            <label class="form-label">
+                商品画像 <span class="required">必須</span>
+            </label>
             <input type="file" name="image" accept="image/*">
             @error('image')
                 <div class="error-message">{{ $message }}</div>
@@ -36,7 +42,9 @@
         </div>
 
         <div class="form-group">
-            <label class="form-label">季節</label>
+            <label class="form-label">
+                季節 <span class="required">必須</span> <span class="season-multiple">複数選択可</span>
+            </label>
             <div class="season-grid">
                 @foreach($seasons as $season)
                     <label class="season-checkbox">
@@ -52,8 +60,10 @@
         </div>
 
         <div class="form-group">
-            <label class="form-label">商品説明</label>
-            <textarea name="description" rows="4">{{ old('description') }}</textarea>
+            <label class="form-label">
+                商品説明 <span class="required">必須</span>
+            </label>
+            <textarea name="description" rows="4" placeholder="商品の説明を入力">{{ old('description') }}</textarea>
             @error('description')
                 <div class="error-message">{{ $message }}</div>
             @enderror
